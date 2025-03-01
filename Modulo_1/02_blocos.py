300d2d084355be2d53784b1a3c73604886c0bcbd
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-01 18:16:53 +0100

    class 2 syntax

---
+++ Modulo_1/02_blocos.py
@@ -0,0 +1,8 @@
+
+#Blocos e identação - identação errada, dá problema de sintaxe
+idade = 25
+
+if idade < 18:
+   print("menor de idade") #Identação executa o que está no bloco
+else:
+   print("maior de idade") #Identação executa o que está no bloco
---
+++ Modulo_1/02_sintaxe.py
@@ -0,0 +1,3 @@
+# COMENTÁRIO - uma só linha
+"""dá para fazer comentários de multiplas linhas
+aqui temos outro comentário"""
