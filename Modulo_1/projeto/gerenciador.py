676b76e21f9c8cfbb647e9c50bf388f4346380fd
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-03 10:28:08 +0100

    added function add_task

--- Modulo_1/projeto/gerenciador.py
+++ Modulo_1/projeto/gerenciador.py
@@ -1,4 +1,17 @@
 # while - Enquanto o usuário nao digitar a opção de sair ele continua rodando
+   
+def adicionar_tarefa(tarefas, nome_tarefa):
+   
+   # chave: tarefa: nome da tarefa 
+   # chave completada: indicar se essa tarefa já foi completada ou não(4,5 do menu)
+   # Como temos 1 ou mais informações vamos usar o dict
+   tarefas = {"tarefa": "nome_tarefa", "completada": False} #como está inserindo, em primeiro momento completada é sempre False
+   tarefas.append(tarefas)
+   # tarefas.append({"tarefa": "nome_tarefa", "completada": False}) > mesmo código acima
+   print(f"A tarefa {nome_tarefa} foi adicionada com sucesso!")
+   return
+
+tarefas = []   
 
 while True:
 
@@ -11,7 +24,21 @@ while True:
    print("6. Sair")
 
    escolha = input("Digite a sua escolha:")
+
+   if escolha == "1":
+      
+
+
+
+
+
    if  escolha == "6":
       break
       
 print("Programa finalizado")
+
+# def adicionarTarefa(newTask):
+#    listaTarefas = newTask
+#    return listaTarefas
+
+
