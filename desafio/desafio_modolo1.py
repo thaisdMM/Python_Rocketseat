497697e5acae0e9fbadcd1f3706052a161f969be
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-10 17:03:13 +0100

    edit contact not ""

--- desafio/desafio_modolo1.py
+++ desafio/desafio_modolo1.py
@@ -1,33 +1,3 @@
-# ### Sobre o desafio
-
-# Nesse desafio desenvolveremos uma agenda para salvar, editar, deletar e marcar um contato como favorito. O resultado da aplicação deve ser apresentado no terminal, assim como foi visto no módulo “Introdução ao Python”.
-# ### Regras da aplicação
-
-# A aplicação deve iniciar mostrando uma lista de opções do que é possível fazer com o app e permitir que o usuário digite uma escolha para iniciar a aplicação.
-
-# Deve ser possível adicionar um contato 
-#> tem que poder ser vazio acho que tinha um codigo=
-
-# O contato pode ter os dados: é um dict 
-
-# Nome
-
-# Telefone
-
-# Email
-
-# Favorito (está opção é para poder marcar um contato como favorito)
-
-# Deve ser possível visualizar a lista de contatos cadastrados
-
-# Deve ser possível editar um contato
-
-# Deve ser possível marcar/desmarcar um contato como favorito
-
-# Deve ser possível ver uma lista de contatos favoritos
-
-# Deve ser possível apagar um contato
-
 def adiciona_contato(lista_contatos, nome_contato, telefone_contato, email_contato):
    contato = {"nome": nome_contato,"telefone": telefone_contato, "email": email_contato, "favorito": False}
 
@@ -51,13 +21,15 @@ def ver_contatos(lista_contatos):
 def editar_contato(lista_contatos, indice_contato, novo_contato, novo_telefone_contato, novo_email_contato):
    indice_contato_ajustado = int(indice_contato) -1
    if indice_contato_ajustado >= 0 and indice_contato_ajustado <len(lista_contatos):
-      lista_contatos[indice_contato_ajustado] ["nome"] = novo_contato
-      lista_contatos[indice_contato_ajustado] ["telefone"] = novo_telefone_contato
-      lista_contatos[indice_contato_ajustado] ["email"] = novo_email_contato
+
       if not novo_contato or novo_contato == " ":
          print("\nO nome do novo contato não pode ser vazio!")
-      else: 
+      else:
+         lista_contatos[indice_contato_ajustado] ["nome"] = novo_contato
+         lista_contatos[indice_contato_ajustado] ["telefone"] = novo_telefone_contato
+         lista_contatos[indice_contato_ajustado] ["email"] = novo_email_contato
          print(f"\nO contato {indice_contato} foi atualizado para {novo_contato} com sucesso!")
+         
    else:
       print("\nNúmero de contato inválido ou não existe.")
    return
