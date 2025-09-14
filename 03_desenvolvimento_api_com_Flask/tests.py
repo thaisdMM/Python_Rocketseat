84f0b72d11360ac15e4aad16f0b3415118a60e58
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-09-14 18:43:02 +0200

    tests Read - get tasks

--- 03_desenvolvimento_api_com_Flask/tests.py
+++ 03_desenvolvimento_api_com_Flask/tests.py
@@ -15,3 +15,20 @@ def test_create_tasks():
     assert "message" in response_json  # message(se existe, não a msg em si)
     assert "id" in response_json  # se volta id, não o numero do id
     tasks.append(response_json["id"])
+
+
+def test_get_tasks():
+    response = requests.get(f"{BASE_URL}/tasks")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "tasks" in response_json
+    assert "total_tasks" in response_json
+
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert task_id == response_json["id"]
