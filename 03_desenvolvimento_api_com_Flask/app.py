a8f1ad84950c737706ccb5c33d06a61528c695b7
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-09-14 13:51:22 +0200

    stating creating task and POST route

--- 03_desenvolvimento_api_com_Flask/app.py
+++ 03_desenvolvimento_api_com_Flask/app.py
@@ -1,17 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+from models.task import Task
 
 app = Flask(__name__)
 
 
-# definindo a rota / é a padrão
-@app.route("/")
-def hello_world():
-    return "Hello World"
+# CRUD
+# CREATE, READ, UPDATE, DELETE
+# Tabela: tarefa
+
+# sem banco de dados, vamos começar com essa lista para organizar os dados(tasks)
+tasks = []
 
 
-@app.route("/about")
-def about():
-    return "Pagina sobre"
+# Create - Olha a criação da tarefa no arquivo da aula no https://editor.swagger.io/
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    # receber os dados do cliente
+    data = request.get_json()
+    print(data)
+    return "Test"
 
 
 # modo de desenvolvimento local
